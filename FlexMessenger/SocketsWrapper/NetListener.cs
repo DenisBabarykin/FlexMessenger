794c7237441f8c0ca864264284328a36e6cf724c
babarykin_denis <babarykin.denis@gmail.com>
2015-04-18 03:50:54 +0300

    SocketsWrapper project created

---
+++ FlexMessenger/SocketsWrapper/NetListener.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocketsWrapper
+{
+    public class NetListener : INetListener
+    {
+        TcpListener tcpListener;
+        public void Start()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Stop()
+        {
+            throw new NotImplementedException();
+        }
+
+        public System.Net.EndPoint LocalEndpoint
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public NetClient AcceptClient()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
